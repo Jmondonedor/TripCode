54d66227dd08cf606f9f4024d87a4c7657809764
gg <gg@hotmail.com>
2023-10-28 12:54:24 -0500

    Correcion pequeña

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ for (let el of imagenes) {
   el.addEventListener("click", (e) => {
     var img1 = e.target.getAttribute("src");
     var aux=getComputedStyle(document.getElementsByClassName("main_2")[0]).getPropertyValue("background");
-    console.log(aux);
     var regex=/images\/([^"]+).jpg/g;
     var img2=aux.match(regex)[0];
     document.querySelector(".main_2").style.backgroundImage = `url(${img1})`;
